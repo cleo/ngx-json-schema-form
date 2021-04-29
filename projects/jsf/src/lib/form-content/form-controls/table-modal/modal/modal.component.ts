afdbefa3c14abedd8f6bd787f0b519e48e77fab9
mdoyle023 <michelledoyle023@gmail.com>
2021-04-29 15:15:23 -0500

    Resize and move modal

--- projects/jsf/src/lib/form-content/form-controls/table-modal/modal/modal.component.ts
+++ projects/jsf/src/lib/form-content/form-controls/table-modal/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
 import { take, takeUntil, tap } from 'rxjs/operators';
 import { ContentBaseComponent } from '../../../content-base.component';
 import { ModalService } from './modal.service';
@@ -11,10 +11,23 @@ import { ModalService } from './modal.service';
 
 export class ModalComponent extends ContentBaseComponent implements AfterViewInit {
   @ViewChild('modal', { static: true }) modalEl: ElementRef;
+  @ViewChild('modalContent', { static: true }) modalContentEl: ElementRef;
+
+  @Output() closeInput: EventEmitter<any> = new EventEmitter();
+  @Output() resizeInput: EventEmitter<any> = new EventEmitter();
 
   shouldHaveFadeInClass = false;
 
-  @Output() closeInput: EventEmitter<any> = new EventEmitter();
+  width: number;
+  minWidth: number;
+  left = 0;
+  previousLeft = 0;
+  top = 0;
+  previousTop = 0;
+  height: number;
+  minHeight: number;
+  draggingCorner = false;
+  draggingWindow = false;
 
   constructor(
     private modalService: ModalService,
@@ -37,6 +50,11 @@ export class ModalComponent extends ContentBaseComponent implements AfterViewIni
     ).subscribe();
 
     this.modalEl.nativeElement.focus();
+
+    this.width = this.modalContentEl.nativeElement.offsetWidth;
+    this.height = this.modalContentEl.nativeElement.offsetHeight;
+    this.minWidth = this.modalContentEl.nativeElement.offsetWidth;
+    this.minHeight = this.modalContentEl.nativeElement.offsetHeight;
   }
 
   onClick(event: MouseEvent): void {
@@ -53,5 +71,67 @@ export class ModalComponent extends ContentBaseComponent implements AfterViewIni
     event.stopPropagation();
 
     this.closeInput.emit();
+  }
+
+  onWindowPress(event: MouseEvent) {
+    this.draggingWindow = true;
+    this.previousLeft = event.clientX;
+    this.previousTop = event.clientY;
+  }
+
+  onWindowDrag(event: MouseEvent) {
+    if (!this.draggingWindow) {
+      return;
+    }
+
+    const offsetX = event.clientX - this.previousLeft;
+    const offsetY = event.clientY - this.previousTop;
+
+    this.left += offsetX;
+    this.top += offsetY;
+    this.previousLeft = event.clientX;
+    this.previousTop = event.clientY;
+  }
+
+  resize(offsetX: number, offsetY: number) {
+    this.width += offsetX;
+    this.height += offsetY;
+
+    if (this.width < this.minWidth) {
+      this.width = this.minWidth;
+    }
+
+    if (this.height < this.minHeight) {
+      this.height = this.minHeight;
+    }
+    this.resizeInput.emit();
+  }
+
+  onCornerClick(event: MouseEvent) {
+    this.draggingCorner = true;
+    this.previousLeft = event.clientX;
+    this.previousTop = event.clientY;
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onCornerMove(event: MouseEvent) {
+    if (!this.draggingCorner) {
+      return;
+    }
+
+    const offsetX = event.clientX - this.previousLeft;
+    const offsetY = event.clientY - this.previousTop;
+
+    this.resize(offsetX, offsetY);
+    this.previousLeft = event.clientX;
+    this.previousTop = event.clientY;
+  }
+
+  @HostListener('document:mouseup', ['$event'])
+  onCornerRelease(event: MouseEvent) {
+    this.draggingWindow = false;
+    this.draggingCorner = false;
   }
 }
