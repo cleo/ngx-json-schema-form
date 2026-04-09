f4c7fdc7cc83fa5f1c3bd109814a3e6c707be198
CleoMBautista <mbautista@cleo.com>
2026-04-09 14:42:55 -0400

    fix: clean up HTML structure and improve formatting across multiple components

--- projects/jsf/src/public_api.ts
+++ projects/jsf/src/public_api.ts
@@ -2,21 +2,21 @@
  * Public API Surface of jsf
  */
 
-// 🔹 Componentes standalone
+// Standalone components
 export * from './lib/jsf.component';
 
-// 🔹 Config / models / contracts
+// Config / models / contracts
 export * from './lib/jsf-config';
 export * from './lib/jsf-schema-data';
 export * from './lib/jsf-json-schema';
 
 export * from './lib/models/form-data-item';
 
-// 🔹 Events
+// Events
 export * from './lib/jsf-event-button';
 export * from './lib/jsf-event-button-target';
 export * from './lib/jsf-template-target';
 export * from './lib/jsf-template-event';
 
-// 🔹 Services (if any are used externally)
+// Services (if any are used externally)
 export * from './lib/form-data-item.service';
